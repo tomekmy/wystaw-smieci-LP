933a158bbe14ea8f69e12c865a12dc612e54fe34
Tomasz Myśliwiec <tomek@mysliwiec.pro>
2018-01-03 23:08:31 +0100

    Change <h2> header to <h1>

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
         >
           <v-flex xs12 class="mt-3">
             <div class="text-xs-center">
-              <h2 class="headline">HARMONOGRAM ODBIORU ODPADÓW W ZIELONCE</h2>
+              <h1 class="headline">HARMONOGRAM ODBIORU ODPADÓW W ZIELONCE</h1>
               <a href="#"><img class="mt-3" src="/static/img/google-play-badge.png" alt="Pobierz aplikację" height="65"></a>
             </div>
           </v-flex>
